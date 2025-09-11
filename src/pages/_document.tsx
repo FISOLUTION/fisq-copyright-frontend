886ef1b34bdff23b6e4a2aa2e1e2c690a1c8d57f
Jaehyun An <uwoobeat@gmail.com>
2025-09-11 15:55:09 +0900

    fix: HTTP 서버로 요청 가능하도록 허용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
