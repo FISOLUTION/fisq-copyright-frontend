a34e27c567f1c2d1706ed69ac9deeda7d01746a3
Jaehyun An <uwoobeat@gmail.com>
2025-11-12 16:40:01 +0900

    fix: 사이드바 헤더와 메인 헤더의 높이 정렬

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -46,16 +46,18 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <Link href="/home" className="flex items-center px-2">
-          <Image
-            src="/logo.png"
-            alt="FISQ Logo"
-            width={100}
-            height={25}
-            className="h-6 w-auto"
-            priority
-          />
-        </Link>
+        <div className="flex h-16 items-center px-2">
+          <Link href="/home">
+            <Image
+              src="/logo.png"
+              alt="FISQ Logo"
+              width={100}
+              height={25}
+              className="h-6 w-auto"
+              priority
+            />
+          </Link>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
