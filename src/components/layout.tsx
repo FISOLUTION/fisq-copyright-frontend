f112a207525c22a96f9ebe89517cc5a683def8f2
Jaehyun An <uwoobeat@gmail.com>
2025-11-13 16:55:12 +0900

    refactor: 레이아웃 헤더 스타일을 dashboard 스타일로 변경

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -24,15 +24,13 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-          </div>
-          <div className="flex flex-1 items-center justify-end px-4">
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <div className="flex flex-1 items-center justify-end">
             <Button
               variant="ghost"
               size="icon"
@@ -43,7 +41,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
             </Button>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4">
           {children}
         </div>
       </SidebarInset>
