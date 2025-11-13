8ba74fd5a94373cfe3f2b1df047efd5f6863e15b
Jaehyun An <uwoobeat@gmail.com>
2025-11-13 17:56:02 +0900

    refactor: action-toolbar에 ButtonGroup 컴포넌트 적용

--- src/components/search/action-toolbar.tsx
+++ src/components/search/action-toolbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
 import { Download, Plus, RotateCcw, Search, Upload, X } from "lucide-react";
 import SingleAddDialog, { FormField } from "./single-add-dialog";
 import ExcelUploadDialog from "./excel-upload-dialog";
@@ -85,39 +86,47 @@ export default function ActionToolbar({
           className="mx-2 data-[orientation=vertical]:h-6"
         />
 
-        <Button size="sm" onClick={() => setSingleAddOpen(true)}>
-          <Plus className="mr-2 h-4 w-4" />
-          단건 추가하기
-        </Button>
+        <ButtonGroup>
+          <ButtonGroup>
+            <Button size="sm" onClick={() => setSingleAddOpen(true)}>
+              <Plus className="mr-2 h-4 w-4" />
+              단건 추가하기
+            </Button>
 
-        <Button size="sm" onClick={() => setExcelUploadOpen(true)}>
-          <Upload className="mr-2 h-4 w-4" />
-          엑셀 업로드
-        </Button>
+            <Button size="sm" onClick={() => setExcelUploadOpen(true)}>
+              <Upload className="mr-2 h-4 w-4" />
+              엑셀 업로드
+            </Button>
+          </ButtonGroup>
 
-        <Button size="sm" onClick={onSearch} disabled={isSearching}>
-          <Search className="mr-2 h-4 w-4" />
-          검색하기
-        </Button>
+          <ButtonGroup>
+            <Button size="sm" onClick={onSearch} disabled={isSearching}>
+              <Search className="mr-2 h-4 w-4" />
+              검색하기
+            </Button>
 
-        <Button size="sm" onClick={onExcelDownload}>
-          <Download className="mr-2 h-4 w-4" />
-          엑셀 다운로드
-        </Button>
+            <Button size="sm" onClick={onExcelDownload}>
+              <Download className="mr-2 h-4 w-4" />
+              엑셀 다운로드
+            </Button>
 
-        <Button size="sm" onClick={onReset}>
-          <RotateCcw className="mr-2 h-4 w-4" />
-          초기화
-        </Button>
+            <Button size="sm" onClick={onReset}>
+              <RotateCcw className="mr-2 h-4 w-4" />
+              초기화
+            </Button>
+          </ButtonGroup>
 
-        <Button
-          size="sm"
-          onClick={onRemoveSelected}
-          disabled={selectedItemsCount === 0}
-        >
-          <X className="mr-2 h-4 w-4" />
-          선택 제거
-        </Button>
+          <ButtonGroup>
+            <Button
+              size="sm"
+              onClick={onRemoveSelected}
+              disabled={selectedItemsCount === 0}
+            >
+              <X className="mr-2 h-4 w-4" />
+              선택 제거
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
       </div>
 
       <SingleAddDialog
